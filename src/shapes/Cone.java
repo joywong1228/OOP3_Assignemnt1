b974d4fc786f2de08ab28dd983b9b6e214b7c2ab
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-06-06 15:14:11 -0600

    Updating Cone, Cylinder and Pyramid classes

--- src/shapes/Cone.java
+++ src/shapes/Cone.java
@@ -1,6 +1,39 @@
 package shapes;
 
-public class Cone
+public class Cone extends Shape
 {
+    private double radius;
+    private double height;
 
+    public Cone (double radius, double height)
+    {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    public double height()
+    {
+        return height;
+    }
+
+    public double radius()
+    {
+        return radius;
+    }
+
+    @Override
+    public double calBaseArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calVolume() {
+        return calBaseArea() * height /3;
+    }
+
+    @Override
+    public String toString() {
+        return "Cone " + height + " " + radius;
+    }
 }
--- src/shapes/Cylinder.java
+++ src/shapes/Cylinder.java
@@ -1,6 +1,39 @@
 package shapes;
 
-public class Cylinder
+public class Cylinder extends Shape
 {
+    private double radius;
+    private double height;
 
+    public Cylinder (double radius, double height)
+    {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    public double height()
+    {
+        return height;
+    }
+
+    public double radius()
+    {
+        return radius;
+    }
+
+    @Override
+    public double calBaseArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calVolume() {
+        return calBaseArea() * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder " + height + " " + radius;
+    }
 }
