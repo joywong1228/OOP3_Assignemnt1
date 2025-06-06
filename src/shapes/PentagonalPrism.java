4c446cd60068d88f185dfe41ca5cc1d627718e72
Lam Dao <lam.dao@edu.sait.ca>
2025-06-06 16:13:37 -0600

    Upload OctagonalPrism and PentagonalPrism class

--- src/shapes/PentagonalPrism.java
+++ src/shapes/PentagonalPrism.java
@@ -1,6 +1,23 @@
 package shapes;
 
-public class PentagonalPrism
+public class PentagonalPrism extends Prism
 {
+    public PentagonalPrism(double height, double side) {
+        super(height, side);
+    }
 
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+        @Override
+    public double calBaseArea() {
+        return side*side*5/4*Math.tan(54/180*Math.PI);
+    }
+
+    @Override
+    public double calVolume() {
+        return getHeight()*calBaseArea();
+    }
 }
