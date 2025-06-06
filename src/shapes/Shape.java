a302eba722533f179a9bd9f913712c07d316ea82
JoyWong <joywong1228@gmail.com>
2025-06-06 14:40:10 -0600

    Update Shape, SquarePrism, TriangularPrism

---
+++ src/shapes/Shape.java
@@ -0,0 +1,20 @@
+package shapes;
+
+/**
+ * Abstract base class for all 3D shapes.
+ */
+public abstract class Shape implements Comparable<Shape> {
+
+    public abstract double height();
+
+    public abstract double radius();
+
+    public abstract double calBaseArea();
+
+    public abstract double calVolume();
+
+    @Override
+    public int compareTo(Shape other) {
+        return Double.compare(this.height(), other.height());
+    }
+}
