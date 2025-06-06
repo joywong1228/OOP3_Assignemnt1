3e40c990458a890f9a774fe3444bc47ae939c6cd
Lam Dao <lam.dao@edu.sait.ca>
2025-06-06 16:12:52 -0600

    Add criteria to compareTo method

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -5,14 +5,26 @@ package shapes;
  */
 public abstract class Shape implements Comparable<Shape> {
 
-    public abstract double height();
+    public abstract double getHeight();
 
     public abstract double calBaseArea();
 
     public abstract double calVolume();
 
     @Override
-    public int compareTo(Shape other) {
-        return Double.compare(this.height(), other.height());
+    public int compareTo(Shape other, String criteria) {
+        int compare = 0;
+        switch (criteria.toLowerCase()) {
+            case "height":
+                compare = Double.compare(this.getHeight(), other.getHeight());
+                break;
+            case "base area":
+                compare = Double.compare(this.calBaseArea(), other.calBaseArea());
+                break;
+            case "volume":
+                compare = Double.compare(this.calVolume(), other.calVolume());
+                break;
+        }
+        return compare;
     }
 }
