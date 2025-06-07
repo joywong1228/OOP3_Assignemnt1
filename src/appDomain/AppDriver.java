5a41a2cd37f138fc11198435ac879da00f62c00b
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-06-07 00:22:35 -0600

    Creating MergeSort and SelectionSort
    App Driver is just for checking the method for now

--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -1,10 +1,42 @@
 package appDomain;
 
+import shapes.*;
+import utilities.MergeSort;
+import utilities.SelectionSort;
+
 public class AppDriver
 {
 
 	public static void main( String[] args )
 	{
+		// for checking only
+		Shape[] shapes = {
+			new Cone(3,5),
+			new Cone(2,10),
+			new Cone(4,2)
+		};
+
+		SelectionSort.sort(shapes, "volume");
+
+		System.out.println("Sorted by volume: ");
+		for (Shape shape: shapes) {
+			System.out.println(shape + "\tVolume: \t" + shape.calVolume());
+		}
+
+		SelectionSort.sort(shapes, "height");
+
+		System.out.println("Sorted by height: ");
+		for (Shape shape: shapes) {
+			System.out.println(shape + "\tHeight: \t" + shape.getHeight());
+		}
+
+		SelectionSort.sort(shapes, "base area");
+
+		System.out.println("Sorted by base area: ");
+		for (Shape shape: shapes) {
+			System.out.println(shape + "\tBase area: \t" + shape.calBaseArea());
+		}
+
 		// TODO Auto-generated method stub
 
 		// refer to demo001 BasicFileIO.java for a simple example on how to
