8fa51707b8a975d4936364aeb4d7e7889eb62352
JoyWong <joywong1228@gmail.com>
2025-06-11 07:59:41 -0600

    bug fix: bubble sort and insertion sort

--- src/utilities/BubbleSort.java
+++ src/utilities/BubbleSort.java
@@ -1,33 +1,26 @@
 package utilities;
 
-import shapes.*;
-import java.util.Comparator;
+import shapes.Shape;
 
 public class BubbleSort {
-
-    public static void sort(Shape[] array, Comparator<Shape> comparator) {
-        int n = array.length;
-        boolean swapped;
+    public static void sort(Shape[] list, String comparator) {
+        int n = list.length;
 
         for (int i = 0; i < n - 1; i++) {
-            swapped = false;
+            boolean swapped = false;
 
             for (int j = 0; j < n - i - 1; j++) {
-                int cmp = (comparator == null)
-                        ? array[j].compareTo(array[j + 1])
-                        : comparator.compare(array[j], array[j + 1]);
-
-                if (cmp < 0) { // DESCENDING ORDER
-                    Shape temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                if (list[j].compareTo(list[j + 1], comparator) < 0) {
+                    // Swap
+                    Shape temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
                     swapped = true;
                 }
             }
 
-            if (!swapped) {
-                break; // No swaps means the array is sorted
-            }
+            // If no elements were swapped in this pass, list is sorted
+            if (!swapped) break;
         }
     }
 }
